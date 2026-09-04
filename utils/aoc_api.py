833076c240ec4aff73c44acb58a1cbec74a53142
greyfood <villeelings@gmail.com>
2026-09-04 03:05:27 +0200

    init module refactored and extended. API WIP

--- utils/aoc_api.py
+++ utils/aoc_api.py
@@ -2,7 +2,8 @@ import requests
 from pathlib import Path
 from http.cookiejar import MozillaCookieJar
 from markdownify import markdownify as md
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
+from bs4.element import PageElement
 class AocAPI():
     def __init__(self, base_url:str="https://adventofcode.com/"):
         self.base_url = base_url
@@ -19,40 +20,68 @@ class AocAPI():
 
     def get_puzzle_instructions(self, day:int, year:int) -> str:
         route = self._year_day_route(day, year)
-        page_html = self._get(route).text
-        soup_html = BeautifulSoup(page_html, features="html.parser")
-        instructions_tag = soup_html.find("article")
-        if instructions_tag is None:
+        response_html = self._get(route).text
+
+        soup_html = self._beautiful_soup(response_html)
+        article_tag = self._bs_find(soup_html, "article")
+        if article_tag is None:
             raise Exception("Couldn't find tag containing puzzle instructions.")
-        return md("".join([str(elem) for elem in instructions_tag.contents]))
+        return md("".join([str(elem) for elem in article_tag.contents]))
 
     def submit_puzzle_answer(self, day:int, year:int, answer) -> bool:
         "TODO: test and integrate"
-        data = {"answer": answer}
-        output = self._post(self._year_day_route(day, year), data)
-        print(output.status_code)
-        return True
+        data = {
+            "answer": answer,
+            "level": day
+        }
+        output = self._post(self._year_day_route(day, year) + "/answer", data)
+
+        response_html = output.text
+        soup_html = self._beautiful_soup(response_html)
+        article_tag = self._bs_find(soup_html, "article")
+        first_paragraph = self._bs_find(article_tag, "p")
+        print(first_paragraph.text)
+        answer_is_correct = "That's not the right answer." not in first_paragraph.text
+        return answer_is_correct
     
     def _get(self, route:str):
-        return requests.get(
+        get_request = requests.get(
             url=self.base_url + route,
             cookies=self.cookies
         )
+        #if get_request.status_code != 200:
+        
+        return get_request
     
     def _post(self, route:str, data):
-        return requests.post(
+        post_request = requests.post(
             url=self.base_url + route,
             cookies=self.cookies,
             data=data
         )
-    def _year_day_route(self, day:int, year:int) -> str:
+
+        #if post_request.status_code != 200:
+
+        return post_request
+    
+    @staticmethod
+    def _year_day_route(day:int, year:int) -> str:
         return f"{year}/day/{day}"
+    
+    @staticmethod
+    def _beautiful_soup(html:str) -> BeautifulSoup:
+        return BeautifulSoup(html, features="html.parser")
     """def _day_path(self, day:int, year:int):
         Path(BASE)"""
 
+    @staticmethod
+    def _bs_find(bs:Tag, tag_label:str) -> Tag:
+        find_results = bs.find(tag_label)
+        if not isinstance(find_results, Tag):
+            raise Exception(f"Excepted to find a single '{tag_label}' tag, but didn't find any.")
+        return find_results
+
 if __name__ == "__main__":
     aoc = AocAPI()
-    print(md(aoc.get_puzzle_instructions(1, 2025)))
-    with open("test.md", 'w') as f:
-        f.write(md(aoc.get_puzzle_instructions(1, 2025)))
+    print(aoc.submit_puzzle_answer(1, 2025, 5))
     
---
+++ utils/init.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+import argparse
+
+from .aoc_api import AocAPI
+def _create_directory(directory_path:Path, create_keep_file=False, exist_ok=False):
+    directory_path.mkdir(exist_ok=exist_ok)
+    if create_keep_file:
+        with open(Path(directory_path, ".keep"), "w") as f:
+            f.write("")
+
+def _string_to_file(contents:str, path:Path) -> None:
+    with open(path, 'w') as f:
+        f.write(contents)
+
+def create_problem_directory(year:int, day:int, root_path:str, download_input:bool, download_task:bool):
+    year_dir_path = Path(root_path, str(year))
+    _create_directory(year_dir_path, exist_ok=True)
+
+    aoc_api = AocAPI()
+    number_string = f"0{day}" if day < 10 else str(day)
+    day_directory_path = Path(year_dir_path, number_string)
+    _create_directory(day_directory_path)
+    _string_to_file("", Path(day_directory_path, "solution.py"))
+    if download_input:
+        puzzle_input_string = aoc_api.get_puzzle_input(day, year)
+        _string_to_file(puzzle_input_string, Path(day_directory_path, "input"))
+        _string_to_file("", Path(day_directory_path, "test_input")) #TODO: parse test input from task description. Not trivial.
+    if download_task:
+        description_directory_path = Path(day_directory_path, "descriptions")
+        _create_directory(description_directory_path)
+        task_description = aoc_api.get_puzzle_instructions(day, year)
+        _string_to_file(task_description, Path(description_directory_path, "part_1.md"))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-y", "--year")
+    parser.add_argument("-d", "--day")
+    parser.add_argument("-i", "--download-inputs", default=True)
+    parser.add_argument("-t", "--download-tasks", default=True)
+    parser.add_argument("-s", "--solutions-dir", default="./solutions")
+    
+    args = parser.parse_args()
+    create_problem_directory(int(args.year), int(args.day), args.solutions_dir, args.download_inputs, args.download_tasks)
